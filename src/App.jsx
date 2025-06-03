7ef72ad35de17fa6df9ccff136bd86ea509d251d
akshar51 <aksharparekh401@gmail.com>
2025-06-03 16:54:54 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Navbar/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
