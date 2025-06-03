f686e27458f5e8f8f63ce3eb3b6ef05ab26c4623
akshar51 <aksharparekh401@gmail.com>
2025-06-03 21:32:07 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,82 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "./components/Navbar";
+import Table from "./components/Table";
+import Form from "./components/Form";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+
 
 const App = () => {
+  let url = "http://localhost:3000/user";
+  const [user, setUser] = useState({});
+  const [list, setList] = useState([]);
+  const [editIdx, setEditIdx] = useState("");
+
+  useEffect(() => {
+    handleFetch();
+  }, []);
+  
+  
+
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (editIdx == "") {
+      await axios.post(url, { ...user, id: String(Date.now()) });
+      toast.success("User added successfully", {});
+    } else {
+      await axios.put(`${url}/${editIdx}`, { ...user });
+      toast.info("User updated successfully", {});
+    }
+
+    handleFetch();
+  };
+
+  const handleFetch = async () => {
+    let res = await axios.get(url);
+    let data = res.data;
+    setList(data);
+  };
+
+  const handleDelete = async (id) => {
+    await axios.delete(`${url}/${id}`);
+    toast.warn("User Deleted!!!", {});
+  };
+
+  const handleEdit = (id) => {
+    let data = list.find((val) => val.id === id)[0];
+    setUser(data);
+    setEditIdx(id);
+  };
+
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Navbar/>}/>
-      </Routes>
+      <Navbar />
+      <Table handleDelete={handleDelete} handleEdit={handleEdit} list={list}/>
+      <Form
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        user={user}
+      />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
 
-const Form = () => {
+const Form = (props) => {
+
+  let {handleChange,handleSubmit,user} = props
   return (
     <>
-      
+      <div className="container">
+        <div className="row">
+          <div className="col-md-7 mx-auto mt-4">
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">
+                  Email : 
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                  onChange={handleChange}
+                  value={user.email || ""}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="password" className="form-label">
+                  Password : 
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="password"
+                  onChange={handleChange}
+                  value={user.password || ""}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
