8fd45b1a3f1b1c3032adfb28e5a4492720eda565
akshar51 <aksharparekh401@gmail.com>
2025-06-04 12:44:32 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,7 @@ const App = () => {
 
   return (
     <>
-      <Routes>
+      <Routes> 
         <Route path="/" element={<Form
         handleChange={handleChange}
         handleSubmit={handleSubmit}
